a94dd797a16ab0a8ed3d3d58cfb9c442615b0abf
Hongjo Lim <hongjo1988@gmail.com>
2023-02-27 17:22:10 -0800

    Created index page

--- portfoilo/portfoilo/urls.py
+++ portfoilo/portfoilo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import jobs.views
 
 urlpatterns = [
+    path('', jobs.views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('list', jobs.views.list, name='list'),
 ]
